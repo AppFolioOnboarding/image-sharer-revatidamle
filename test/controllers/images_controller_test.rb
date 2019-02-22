67bdc3021f788318751043215bfc6b7007f6b8b2
Revati Damle <revati.damle@appfolio.com>
2019-02-21 16:38:42 -0800

    rd - Shows up all the saved images on the homepage

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
-class ImagesControllerTest < ActionDispatch::IntegrationTest
+#
+# class ImagesControllerTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
+
+
+class ImagesControllerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
